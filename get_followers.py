5a387667281eb1c2fef15ce0a6884bbc1f1f63a0
brambilla <brambilla@DSPolimi.como.polimi.it>
2018-07-13 08:58:56 +0200

    fixed script for followers collection

--- get_followers.py
+++ get_followers.py
@@ -3,6 +3,7 @@ import csv
 import json
 import pandas as pd
 import time
+import sys
 
 #login
 path_credentials = '../credentials.json' #complete path of twitter credentials
@@ -14,44 +15,63 @@ def login():
     auth.set_access_token(keys['access_token'], keys['access_token_secret'])
     twitter = tweepy.API(auth, wait_on_rate_limit=False)
     return twitter
-	
+
+
 def limit_handled(cursor):
-    while True:
-        try:
-            yield cursor.next()
-        except tweepy.RateLimitError:
-	    print 'API Rate Limit exceeded. Waiting...'
-            time.sleep(15 * 60)
+	while True:
+		try:
+			yield cursor.next()
+		except tweepy.RateLimitError:
+			print 'API Rate Limit exceeded. Waiting...'
+			time.sleep(15 * 60)
+		except tweepy.error.TweepError:
+			print 'Connection aborted by peer. Waiting...'
+			time.sleep(5 * 60)
 
 # login to API			
 twitterAPI = login()
+
+# select source to gather followers
+source = sys.argv[1] # it can be seed or a number (test number)
 domain = 'finance_20'
-path = 'graphs/data-seed/{}/'.format(domain)
 
-seed_list = list(pd.read_csv(path+'user.csv')['id_user'])
-ofile = open(path+'follower_data.csv', 'w')
-ofile2 = open(path+'follower.csv', 'w')
+if source == 'seed':
+	path = 'graphs/data-seed/{}/'.format(domain)
+	userlist = list(pd.read_csv(path+'user.csv', sep='\t')['id_user'])[14:]
+	ofile2 = open(path+'follower.csv', 'a')
+else:
+	n_test = int(source)
+	path = 'graphs/data-random/'
+	alltestusers = pd.read_csv(path+'user.csv', sep='\t')
+	userlist = list(alltestusers[alltestusers['n_test'] == n_test]['id_user'][:56].unique())
+	ofile2 = open(path+'follower_{}.csv'.format(n_test), 'a')
 
-writer = csv.writer(ofile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-writer.write(['id_user','screen_name','followers','following','lang','location','created_at','link_img'])
+
+#ofile = open(path+'follower_data.csv', 'w')
+#writer = csv.writer(ofile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+#writer.writerow(['id_user','screen_name','followers','following','lang','location','created_at','link_img'])
 
 writer2 = csv.writer(ofile2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-writer2.write(['id_follower','id_followed'])
+writer2.writerow(['id_follower','id_followed'])
 
-allusers = list(pd.read_csv(path+'user.csv')['id_user'])
-start = time.time()
-for id_seed in seed_list:
-	print 'Followers of '+str(id_seed)
-	for follower in limit_handled(tweepy.Cursor(twitterAPI.followers, user_id=id_seed).items()):
-		# to avoid duplicate metadata
-		if follower.id not in allusers:
-			f_row = [follower.id, follower.screen_name, follower.followers_count, follower.friends_count, follower.lang, follower.location, follower.created_at,follower.profile_image_url]
-			writer.writerow(f_row)
-			allusers.append(follower.id)
-			
+MAX_FOLLOWERS = 250000
+processed = 0 # print percentage variable
+startall = time.time()
+for id_user in userlist:
+	start = time.time()
+	print 'Followers of '+str(id_user)
+
+	n_curr_followers = 0
+	for follower in limit_handled(tweepy.Cursor(twitterAPI.followers_ids, user_id=id_user).items()):	
 		# store follow relationship
-		writer2.writerow([follower.id, id_seed])
-	print 'Time needed: {} s'.format(time.time()-start)
+		writer2.writerow([follower, id_user])
 
-print 'Overall time needed: {} min'.format((time.time()-start)/60)
+		n_curr_followers += 1
+		if n_curr_followers >= MAX_FOLLOWERS:
+			break
+
+	processed = processed + 1
+	print 'Completion: {:.1f} %'.format(float(processed)*100/len(userlist))
+
+print 'Overall time needed: {} min'.format((time.time()-startall)/60)
 	
