3d88dfbb9e84071650a9537bc07bcee7799d35d8
brambilla <brambilla@DSPolimi.como.polimi.it>
2018-07-19 18:37:58 +0200

    followers network features performance + template for SNA

--- graphs/fix_edgelist.py
+++ graphs/fix_edgelist.py
@@ -1,4 +1,4 @@
-with open('finance_20/random-test/test-network-features/social_network.edgelist', 'w') as edgelistfile:
-	for line in open('finance_20/random-test/test-network-features/social_network_1.edgelist'):
+with open('finance_20/random-test/test-network-features/temp.edgelist', 'w') as edgelistfile:
+	for line in open('finance_20/random-test/test-network-features/social_network_1_follower.edgelist'):
 		if line[0] != '#':
 			edgelistfile.write(line)
